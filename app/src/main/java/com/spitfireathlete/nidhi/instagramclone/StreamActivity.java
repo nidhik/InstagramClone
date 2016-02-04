ecea4f256789eb37b4983cb20fe131e8ed3fa8f2
nidhik <nidhikulkarni82@gmail.com>
2016-02-03 23:05:35 -0800

    Add android-async-http and the Picasso libraries.

--- app/src/main/java/com/spitfireathlete/nidhi/instagramclone/StreamActivity.java
+++ app/src/main/java/com/spitfireathlete/nidhi/instagramclone/StreamActivity.java
@@ -1,11 +1,7 @@
 package com.spitfireathlete.nidhi.instagramclone;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -15,17 +11,7 @@ public class StreamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stream);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
